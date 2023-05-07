d4b894d12a9babf1c688fba4fa5b1ccba55f0c2f
FFMbyBicycle <unknown>
2023-05-07 08:34:31 +0200

    Add support for older GoPro camera recordings

--- gopro_overlay/filenaming.py
+++ gopro_overlay/filenaming.py
@@ -17,6 +17,9 @@ class Encoding(Enum):
             return Encoding.AVC
         elif letter == "X":
             return Encoding.HEVC
+        # handling of older GoPro videos
+        elif letter == "OPR" or letter == "P":
+            return Encoding.AVC
         else:
             raise ValueError(f"Unknown encoding letter {letter}")
 
@@ -44,17 +47,27 @@ class GoProFile:
         self.encoding = Encoding.from_letter(match.group(1))
         self.letter = match.group(1)
         self.recording = int(match.group(3))
-        self.sequence = int(match.group(2))
+        if match.group(2):
+          self.sequence = int(match.group(2))
+        # Older GoPro videos don't have this matching group, we have to skip
+        else:
+          self.sequence = 0
 
     @staticmethod
     def is_valid_filepath(f: Path):
-        return re.search(r"^G([HX])(\d{2})(\d{4}).MP4", f.name)
+        return re.search(r"^G([OPRHX]{1,4})(\d{0,2})(\d{4}).MP4", f.name)
 
     def related_files(self, d: Path, listdir=os.listdir):
-        find = re.compile(r"G{l}\d\d{n}\.MP4".format(
-            l=self.letter,
-            n=f"{self.recording:04}"
-        ))
+        if self.letter == "H" or self.letter == "X":
+          find = re.compile(r"G{l}\d\d{n}\.MP4".format(
+              l=self.letter,
+              n=f"{self.recording:04}"
+          ))
+        # handling of older GoPro videos
+        elif self.letter == "OPR" or self.letter == "P":
+          find = re.compile(r"(GP|GOPR)(\d\d)*{n}\.MP4".format(
+              n=f"{self.recording:04}"
+          ))
 
         potentials = [ d / name for name in listdir(d) ]
 
